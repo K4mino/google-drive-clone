5cf3561adb307e4746ee3dd182dc368804d4bb62
Абдижаббаров Ержан Алматулы <y.abdizhabbarov@ffin.life>
2025-03-19 08:54:21 +0500

    progress bar

--- src/app/f/[folderId]/file-row.tsx
+++ src/app/f/[folderId]/file-row.tsx
@@ -25,7 +25,7 @@ export function FileRow(props: { file: typeof files_table.$inferSelect }) {
 
                 return newProgress
             })
-        }, 300)
+        }, 200)
         try {
             await deleteFile(fileId)
             clearInterval(interval)
@@ -81,7 +81,7 @@ export function FolderRow(props: { folder: typeof folders_table.$inferSelect }) 
 
                 return newProgress
             })
-        }, 300)
+        }, 100)
         try {
             await deleteFolder(folderId)
             clearInterval(interval)
